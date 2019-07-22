2ce91ed2b569efb2a6e19ff853fd4c3c700f25ad
18300603955 <oceaniu@163.com>
2019-07-22 17:25:17 +0800

    admin

---
+++ src/main/java/com/niu/config/WebSecurityConfig.java
@@ -0,0 +1,39 @@
+package com.niu.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * Created by ami on 2019/7/22.
+ */
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("$2a$10$RMuFXGQ5AtH4wOvkUqyvuecpqUSeoxZYqilXzbz50dceRsga.WYiq")
+                .roles("admin")
+                .and()
+                .withUser("sang")
+                .password("$2a$10$RMuFXGQ5AtH4wOvkUqyvuecpqUSeoxZYqilXzbz50dceRsga.WYiq")
+                .roles("user");
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().permitAll();
+    }
+}
--- src/main/java/com/niu/controller/GreetingController.java
+++ src/main/java/com/niu/controller/GreetingController.java
@@ -1,17 +1,33 @@
 package com.niu.controller;
 
+import com.niu.model.Chat;
 import com.niu.model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+
+import java.security.Principal;
 
 /**
  * Created by ami on 2019/7/22.
  */
 @Controller
 public class GreetingController {
+    @Autowired
+    SimpMessagingTemplate messagingTemplate;
 
     @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
     public Message greeting(Message message)throws Exception{
         return message;
+    }
+    @MessageMapping("/chat")
+    public void chat(Principal principal, Chat chat) {
+        String from = principal.getName();
+        chat.setFrom(from);
+        messagingTemplate.convertAndSendToUser(chat.getTo(),
+                "/queue/chat", chat);
     }
 }
